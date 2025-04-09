5aa505ea5fa8585be31bc3ef07c1b32cc5796199
Silviu Ojog <sil.ojog@gmail.com>
2025-04-09 20:39:34 +0300

    Insert Comenzi

---
+++ Curs5/comenzi_sql.sql
@@ -0,0 +1,19 @@
+USE universitate;
+
+INSERT INTO profesor SET idprofesor=1, nume="Popescu", prenume="Gica", adresa=1, data_nasterii="1964-02-01", grad="I" ;
+INSERT INTO profesor SET idprofesor=2, nume="Marin", prenume="Andreea", adresa=1, data_nasterii="1989-12-30", grad="II" ;
+INSERT INTO profesor SET idprofesor=3, nume="Florescu", prenume="Florina", adresa=1, data_nasterii="1989-12-10", grad="II" ;
+INSERT INTO profesor SET idprofesor=4, nume="Ionescu", prenume="Ioan", adresa=1, data_nasterii="1969-12-09", grad="III" ;
+INSERT INTO profesor SET idprofesor=5, nume="Marinesc", prenume="Marin", adresa=1, data_nasterii="1990-10-30", grad="II" ;
+INSERT INTO profesor SET idprofesor=6, nume="Baturalp", prenume="Ece", adresa=1, data_nasterii="1968-01-07", grad="I" ;
+INSERT INTO profesor SET idprofesor=7, nume="Hagele", prenume="Kate", adresa=1, data_nasterii="1985-04-14", grad="I" ;
+INSERT INTO profesor SET idprofesor=8, nume="Vernekar", prenume="Baljiwan", adresa=1, data_nasterii="1948-03-07", grad="I" ;
+INSERT INTO profesor SET idprofesor=9, nume="Wilson", prenume="Isabella", adresa=1, data_nasterii="1988-05-13", grad="I" ;
+INSERT INTO profesor SET idprofesor=10, nume="Yanovich", prenume="Ievfrosiniya", adresa=1, data_nasterii="1950-12-30", grad="I" ;
+INSERT INTO profesor SET idprofesor=11, nume="da Rocha", prenume="Acilino", adresa=1, data_nasterii="1946-05-23", grad="I" ;
+INSERT INTO profesor SET idprofesor=12, nume="Hieber", prenume="Margrit", adresa=1, data_nasterii="2000-07-12", grad="I" ;
+INSERT INTO profesor SET idprofesor=13, nume="Macdonald", prenume="Brielle", adresa=1, data_nasterii="1984-12-20", grad="I" ;
+INSERT INTO profesor SET idprofesor=14, nume="Rajala", prenume="Matilda", adresa=1, data_nasterii="1961-08-13", grad="I" ;
+INSERT INTO profesor SET idprofesor=15, nume="Nielsen", prenume="Freja", adresa=1, data_nasterii="1994-06-14", grad="I" ;
+INSERT INTO profesor SET idprofesor=21, nume="Guido", prenume="Van Rossum", adresa=1, data_nasterii="1956-01-31", grad="I" ;
+SELECT * FROM profesor;
